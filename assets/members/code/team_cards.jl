6f5fde485c7dc90c40fef2fa287ea2e25220558c
lurixM <ninj24o@gmail.com>
2021-11-01 09:06:59 +0000

    Deploying to gh-pages from  @ f7370a346d76a97b47f0c064b8e45a60ba5f4643 🚀

--- assets/members/code/team_cards.jl
+++ assets/members/code/team_cards.jl
@@ -5,5 +5,10 @@ using CSV
 
 person_list = readdir("./_assets/team/current")
 for person in person_list
-    person_data_reader = CSV.File(string("./_assets/team/current/", person, "/profile_info.csv"))
+    person_data_reader = CSV.File(string("./_assets/team/current/", person, "/profile_info.csv"), delim=";")
+    for row in person_data_reader
+        """
+        \\currentCard{current/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}
+        """ |> print
+    end
 end
