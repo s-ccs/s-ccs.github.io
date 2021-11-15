21bc2ff4a05a7fe63c6d81756965d77128c60918
Moritz <ninj24o@gmail.com>
2021-11-15 09:33:31 +0100

    removed __site from repo

--- __site/libs/nav/hamburger.js
+++
@@ -1,8 +0,0 @@
-function toggleHamburger() {
-    var status = document.getElementById("hamburgerLinks");
-    if(status.className === "invisible-hamburger-links") {
-        status.className = "visible-hamburger-links";
-    } else {
-        status.className = "invisible-hamburger-links";
-    }
-}
