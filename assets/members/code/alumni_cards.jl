bdd9dfda37fbf2da81016eed28dedd7ed28b9f8a
lurixM <ninj24o@gmail.com>
2021-11-28 15:48:04 +0000

    Deploying to gh-pages from  @ 51a2e5f8f0c51a5d4df4778c3e5035bcb8a2d589 🚀

--- assets/members/code/alumni_cards.jl
+++ assets/members/code/alumni_cards.jl
@@ -6,9 +6,8 @@ using CSV
 person_list = readdir("./_assets/team/alumni")
 for person in person_list
     person_data_reader = CSV.File(string("./_assets/team/alumni/", person, "/profile_info.csv"), delim=";")
-    for row in person_data_reader
-        """
-        \\currentCard{alumni/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}
-        """ |> print
-    end
+    row = person_data_reader[1]
+    """
+    \\currentCard{alumni/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}
+    """ |> print
 end
--- assets/members/code/team_cards.jl
+++ assets/members/code/team_cards.jl
@@ -6,9 +6,8 @@ using CSV
 person_list = readdir("./_assets/team/current")
 for person in person_list
     person_data_reader = CSV.File(string("./_assets/team/current/", person, "/profile_info.csv"), delim=";")
-    for row in person_data_reader
-        """
-        \\currentCard{current/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}
-        """ |> print
-    end
+    row = person_data_reader[1]
+    """
+    \\currentCard{current/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}
+    """ |> print
 end
