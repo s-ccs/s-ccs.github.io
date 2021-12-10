d7658188f99e3196312908b463f3a1176c5af23d
lurixM <ninj24o@gmail.com>
2021-12-10 16:54:09 +0000

    Deploying to gh-pages from  @ 724bf51e4d5b6ce7d721437bdbfac3b244287fae 🚀

--- assets/members/code/alumni_cards.jl
+++ assets/members/code/alumni_cards.jl
@@ -2,12 +2,19 @@
 #hideall
 import CSV
 using CSV
+using Random
+colors = ["orange", "blue", "green"]
 
 person_list = readdir("./_assets/team/alumni")
 for person in person_list
+    if isempty(colors)
+        global colors = ["orange", "blue", "green"]
+    end
+    color = rand(colors)
+    global colors = filter(e->e!=color, colors)
     person_data_reader = CSV.File(string("./_assets/team/alumni/", person, "/profile_info.csv"), delim=";")
     row = person_data_reader[1]
     """
-    \\currentCard{alumni/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}
+    \\currentCard{alumni/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}{$(color)}
     """ |> print
 end
--- assets/members/code/team_cards.jl
+++ assets/members/code/team_cards.jl
@@ -2,12 +2,19 @@
 #hideall
 import CSV
 using CSV
+using Random
+colors = ["orange", "blue", "green"]
 
 person_list = readdir("./_assets/team/current")
 for person in person_list
+    if isempty(colors)
+        global colors = ["orange", "blue", "green"]
+    end
+    color = rand(colors)
+    global colors = filter(e->e!=color, colors)
     person_data_reader = CSV.File(string("./_assets/team/current/", person, "/profile_info.csv"), delim=";")
     row = person_data_reader[1]
     """
-    \\currentCard{current/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}
+    \\currentCard{current/$(person)}{$(row.title)}{$(row.name)}{$(row.hobbys)}{$(row.contact)}{$(color)}
     """ |> print
 end
