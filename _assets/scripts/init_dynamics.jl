d2d0c368d598c4d68e1019200bb791d2fda6c122
Moritz <ninj24o@gmail.com>
2021-12-29 16:16:43 +0100

    added papers system, documentation will follow

--- _assets/scripts/init_dynamics.jl
+++ _assets/scripts/init_dynamics.jl
@@ -2,3 +2,5 @@ include("generate_side_nav.jl")
 @info "Generated side nav"
 include("generate_toc_pages.jl")
 @info "Generated TOC pages"
+include("generate_papers.jl")
+@info "Generated 'Papers' page"
