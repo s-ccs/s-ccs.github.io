a1c3e74e958015149dfaebcf6472dd9d96e43036
lurixM <ninj24o@gmail.com>
2022-01-06 15:20:31 +0000

    Deploying to gh-pages from  @ e6f017d14f82086cf7d3c58d14633a647d3d711b 🚀

---
+++ libs/modal/modal-thesisart.js
@@ -0,0 +1,5 @@
+var image = document.getElementsByClassName('thesis-art');
+var modal = document.getElementsByClassName('thesis-art-modal')[0];
+var person = document.getElementById("title");
+
+console.log(person)
