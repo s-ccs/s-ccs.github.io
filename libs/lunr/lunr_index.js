be3b35c7be186623d6c3149a3f3d98f8dc065c3b
Moritz <ninj24o@gmail.com>
2022-01-30 13:41:50 +0100

    deleted branch content, to be regenerated

--- libs/lunr/lunr_index.js
+++
@@ -1,2 +0,0 @@
-const LUNR_DATA = {"version":"2.3.9","fields":["d","k","b"],"fieldVectors":[],"invertedIndex":[],"pipeline":["stemmer"]};
-const PREVIEW_LOOKUP = {};
--- libs/parallax/parallax.js
+++
@@ -1,5 +0,0 @@
-window.onscroll = function(){parallax()}
-
-function parallax() {
-    document.getElementById('title-image').setAttribute('style', `background-position-y: ${document.documentElement.scrollTop * 0.4}px`)
-}
