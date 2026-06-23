a5fa19b672b2ed023b7bd7fbec347e76655d4355
behinger <science@benediktehinger.de>
2026-06-23 09:14:23 +0000

    Deploying to gh-pages from  @ ff7955b7bf03f84f782b672b12015b0b8a41b2c8 🚀

--- previews/PR103/assets/scripts/init_dynamics.jl
+++
@@ -1,6 +0,0 @@
-include("generate_side_nav.jl")
-@info "Generated side nav"
-include("generate_toc_pages.jl")
-@info "Generated TOC pages"
-include("generate_papers.jl")
-@info "Generated 'Papers' page"
